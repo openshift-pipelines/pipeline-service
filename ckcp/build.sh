5e86616a7b66594bd76299d1220dab42edffe623
Guillaume Rose <gurose@redhat.com>
2022-02-14 14:12:59 +0100

    Bump kcp

--- ckcp/build.sh
+++ ckcp/build.sh
@@ -2,6 +2,6 @@
 
 set -exuo pipefail
 
-IMAGE="$KO_DOCKER_REPO/ckcp:dfc490d656822da51234c9c18678c3a0c7952c0d"
+IMAGE="$KO_DOCKER_REPO/ckcp:986710c754ed0dac9ae1525661de931e5dd7c0cc"
 docker build -t "$IMAGE" docker/ckcp/
 docker push "$IMAGE"
