970fcbdb0cee96272346df627d0c13dbcd3e4fb0
bnallapeta <bnr@redhat.com>
2022-03-08 12:51:41 +0530

    Modified ckcp/run.sh to use kustomize instead of git clone and applying patch files

--- ckcp/run.sh
+++ ckcp/run.sh
@@ -5,6 +5,8 @@ set -exuo pipefail
 
 KUBECONFIG="${KUBECONFIG:-$HOME/.kube/config}"
 export KUBECONFIG=$KUBECONFIG
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null ; pwd)"
+KUBECONFIG_KCP="$SCRIPT_DIR/work/kubeconfig/admin.kubeconfig"
 
 #create ns, sa, deployment and service resources
 #check if namespace and serviceaccount exists; if not, create them
@@ -31,8 +33,8 @@ podname=$(kubectl get pods -n ckcp -l=app='kcp-in-a-pod' -o jsonpath='{.items[0]
 kubectl wait --for=condition=Ready pod/$podname -n ckcp --timeout=300s
 
 #copy the kubeconfig of kcp from inside the pod onto local filesystem
-rm -f kubeconfig/admin.kubeconfig
-kubectl cp ckcp/$podname:/workspace/.kcp/admin.kubeconfig kubeconfig/admin.kubeconfig
+rm -f work/kubeconfig/admin.kubeconfig
+kubectl cp ckcp/$podname:/workspace/.kcp/admin.kubeconfig work/kubeconfig/admin.kubeconfig
 
 #check if external ip is assigned and replace kcp's external IP in the kubeconfig file
 while [ "$(kubectl get service ckcp-service -n ckcp -o jsonpath='{.status.loadBalancer.ingress[0]}')" == "" ]; do
@@ -44,26 +46,26 @@ done
 
 external_ip=$(kubectl get service ckcp-service -n ckcp -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 external_ip+=$(kubectl get service ckcp-service -n ckcp -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-sed -i "s/localhost/$external_ip/g" kubeconfig/admin.kubeconfig
+sed -i "s/localhost/$external_ip/g" $KUBECONFIG_KCP
 
-KUBECONFIG=kubeconfig/admin.kubeconfig kubectl config set-cluster admin --insecure-skip-tls-verify=true
+KUBECONFIG=$KUBECONFIG_KCP kubectl config set-cluster admin --insecure-skip-tls-verify=true
 
-KUBECONFIG=kubeconfig/admin.kubeconfig kubectl config set-cluster admin --insecure-skip-tls-verify=true
+KUBECONFIG=$KUBECONFIG_KCP kubectl config set-cluster admin --insecure-skip-tls-verify=true
 
 #make sure access to kcp-in-a-pod is good
-until KUBECONFIG=kubeconfig/admin.kubeconfig kubectl api-resources
+until KUBECONFIG=$KUBECONFIG_KCP kubectl api-resources
 do
   sleep 5
   echo "Try again"
 done
 
-kubectl create secret generic ckcp-kubeconfig -n ckcp --from-file kubeconfig/admin.kubeconfig
+kubectl create secret generic ckcp-kubeconfig -n ckcp --from-file $KUBECONFIG_KCP
 
-KUBECONFIG=kubeconfig/admin.kubeconfig kubectl create -f ../workspace.yaml
+KUBECONFIG=$KUBECONFIG_KCP kubectl create -f ../workspace.yaml
 
 #test the registration of a Physical Cluster
 curl https://raw.githubusercontent.com/kcp-dev/kcp/948dbe9565cc7da439c698875ca1fa78350c4530/contrib/examples/cluster.yaml > cluster.yaml
-sed -e 's/^/    /' $KUBECONFIG | cat cluster.yaml - | KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f -
+sed -e 's/^/    /' $KUBECONFIG | cat cluster.yaml - | KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f -
 
 echo "kcp is ready inside a pod and is synced with cluster 'local' and deployment.apps,pods,services and secrets"
 
@@ -75,20 +77,20 @@ else
   do
     if [ $arg == "pipelines" ]; then
       echo "Arg $arg passed. Installing pipelines in ckcp"
-      if [[ ! -d ./pipeline ]]
-      then
-        git clone git@github.com:tektoncd/pipeline.git
-        (cd ./pipeline && git checkout v0.32.0)
-
-        # Conversion is not working yet on KCP
-        (cd pipeline && git apply ../../remove-conversion.patch)
-
-        # Enable OCI bundles
-        (cd pipeline && git apply ../../oci-bundle.patch)
-
-        # Enable artifact PVC volume
-        (cd pipeline && git apply ../../pvc.patch)
-      fi
+#      if [[ ! -d ./pipeline ]]
+#      then
+#        git clone git@github.com:tektoncd/pipeline.git
+#        (cd ./pipeline && git checkout v0.32.0)
+#
+#        # Conversion is not working yet on KCP
+#        (cd pipeline && git apply ../../remove-conversion.patch)
+#
+#        # Enable OCI bundles
+#        (cd pipeline && git apply ../../oci-bundle.patch)
+#
+#        # Enable artifact PVC volume
+#        (cd pipeline && git apply ../../pvc.patch)
+#      fi
 
       #clean up old pods if any in kcpe2cca7df639571aaea31e2a733771938dc381f7762ff7a077100ffad ns
       KCPNS=$(kubectl get namespace kcpe2cca7df639571aaea31e2a733771938dc381f7762ff7a077100ffad --ignore-not-found);
@@ -98,33 +100,35 @@ else
       fi;
 
       #install namespaces in ckcp
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl create namespace default
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl create namespace tekton-pipelines
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl create namespace default
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl create namespace tekton-pipelines
 
       #install pipelines CRDs in ckcp
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f pipeline/config/300-pipelinerun.yaml
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f pipeline/config/300-taskrun.yaml
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f pipeline/config/300-pipelinerun.yaml
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f pipeline/config/300-taskrun.yaml
 
       # will go away with v1 graduation
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f pipeline/config/300-run.yaml
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f pipeline/config/300-resource.yaml
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f pipeline/config/300-condition.yaml
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f pipeline/config/300-run.yaml
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f pipeline/config/300-resource.yaml
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f pipeline/config/300-condition.yaml
 
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply $(ls pipeline/config/config-* | awk ' { print " -f " $1 } ')
+#      KUBECONFIG=$KUBECONFIG_KCP kubectl apply $(ls pipeline/config/config-* | awk ' { print " -f " $1 } ')
 
-      kubectl delete namespace cpipelines || true;
+      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -k ../gitops/tekton-pipeline/overlays/patched
 
-      echo "creating namespace cpipelines";
-      kubectl create namespace cpipelines;
+      kubectl delete namespace pipelines || true;
 
-      kubectl create secret generic ckcp-kubeconfig -n cpipelines --from-file kubeconfig/admin.kubeconfig -o yaml
-      kubectl apply -f gitops/cpipelines/base/deployment.yaml
+      echo "creating namespace pipelines";
+      kubectl apply -f ../gitops/pipelines/base/namespace.yaml;
 
-      cplpod=$(kubectl get pods -n cpipelines -o jsonpath='{.items[0].metadata.name}')
-      kubectl wait --for=condition=Ready pod/$cplpod -n cpipelines --timeout=300s
+      kubectl create secret generic ckcp-kubeconfig -n pipelines --from-file $KUBECONFIG_KCP -o yaml
+      kubectl apply -f ../gitops/pipelines/base/deployment.yaml
+
+      cplpod=$(kubectl get pods -n pipelines -o jsonpath='{.items[0].metadata.name}')
+      kubectl wait --for=condition=Ready pod/$cplpod -n pipelines --timeout=300s
       sleep 30
       #print the pod running pipelines controller
-      KUBECONFIG=$KUBECONFIG kubectl get pods -n cpipelines
+      KUBECONFIG=$KUBECONFIG kubectl get pods -n pipelines
 
     elif [ $arg == "triggers" ]; then
       echo "Arg triggers passed. Installing triggers in ckcp"
@@ -146,15 +150,15 @@ else
       fi
 
       #create secrets for event listener and interceptors so that they can talk to KCP
-      kubectl create secret generic kcp-kubeconfig --from-file=kubeconfig=kubeconfig/admin.kubeconfig --dry-run=client -o yaml | KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f -
-      kubectl create secret generic kcp-kubeconfig -n tekton-pipelines --from-file=kubeconfig=kubeconfig/admin.kubeconfig --dry-run=client -o yaml | KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f -
+      kubectl create secret generic kcp-kubeconfig --from-file=kubeconfig=$KUBECONFIG_KCP --dry-run=client -o yaml | KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f -
+      kubectl create secret generic kcp-kubeconfig -n tekton-pipelines --from-file=kubeconfig=$KUBECONFIG_KCP --dry-run=client -o yaml | KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f -
 
       kubectl delete namespace ctriggers || true;
       echo "creating namespace ctriggers";
       kubectl create namespace ctriggers;
 
       #create secret for ctriggers namespace on the physical cluster so that triggers controller deployment can use it
-      kubectl create secret generic ckcp-kubeconfig -n ctriggers --from-file kubeconfig/admin.kubeconfig --dry-run=client -o yaml | kubectl apply -f -
+      kubectl create secret generic ckcp-kubeconfig -n ctriggers --from-file $KUBECONFIG_KCP --dry-run=client -o yaml | kubectl apply -f -
       kubectl apply -f config/triggers-deployment.yaml
 
       ctrpod=$(kubectl get pods -n ctriggers -o jsonpath='{.items[0].metadata.name}')
@@ -163,15 +167,15 @@ else
       #print the pod running pipelines controller
       KUBECONFIG=$KUBECONFIG kubectl get pods -n ctriggers
 
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply $(ls triggers/config/300-* | awk ' { print " -f " $1 } ')
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply $(ls triggers/config/config-* | awk ' { print " -f " $1 } ')
+      KUBECONFIG=$KUBECONFIG_KCP kubectl apply $(ls triggers/config/300-* | awk ' { print " -f " $1 } ')
+      KUBECONFIG=$KUBECONFIG_KCP kubectl apply $(ls triggers/config/config-* | awk ' { print " -f " $1 } ')
 
-      (cd triggers && KUBECONFIG=../kubeconfig/admin.kubeconfig ko apply -f config/interceptors)
+      (cd triggers && KUBECONFIG=../$KUBECONFIG_KCP ko apply -f config/interceptors)
 
       echo "kubectl get namespaces | grep -i kcp"
       KUBECONFIG=$KUBECONFIG kubectl get namespaces | grep -i kcp
 
-      KUBECONFIG=kubeconfig/admin.kubeconfig kubectl apply -f triggers/examples/v1beta1/github/
+      KUBECONFIG=$KUBECONFIG_KCP kubectl apply -f triggers/examples/v1beta1/github/
 
       echo "Print Interceptor and Event Listener resources in the physical cluster"
       KUBECONFIG=$KUBECONFIG kubectl -n kcpa9f18e6516b976c21e45eb38fd4291927a3c9dd86fda1b7b7c03ead1 get deploy,pods
