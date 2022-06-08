61d80d482408bde4274b4e3056c325c30744850e
Romain Arnaud <rarnaud@redhat.com>
2022-06-08 08:27:30 -0400

    Refactor cluster-setup install script

--- images/cluster-setup/install.sh
+++ images/cluster-setup/install.sh
@@ -20,20 +20,58 @@ set -o pipefail
 
 usage() {
 
-    printf "Usage: DATA_DIR="/workspace" ./register.sh \n\n"
+  printf "
+Usage:
+    %s [options]
 
-    # Parameters
-    printf "The following parameters need to be passed to the script: \n"
-    printf "DATA_DIR: the location of the cluster files \n"
+Deploy Pipelines Service on the clusters as per the configuration in
+WORKSPACE_DIR.
+
+Optional arguments:
+    -w, --workspace-dir WORKSPACE_DIR
+        Location of the folder holding the clusters configuration.
+        Default: \$WORKSPACE_DIR or current directory if the environment
+        variable is unset.
+    -d, --debug
+        Activate tracing/debug mode.
+    -h, --help
+        Display this message.
+
+Example:
+    %s --workspace_dir WORKSPACE_DIR
+" "${0##*/}" "${0##*/}" >&2
 }
 
-prechecks () {
-    DATA_DIR=${DATA_DIR:-}
-    if [[ -z "${DATA_DIR}" ]]; then
-        printf "DATA_DIR not set\n\n"
-        usage
-	      exit 1
-    fi
+parse_args() {
+  WORKSPACE_DIR="${WORKSPACE_DIR:-$PWD}"
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    -w | --workspace-dir)
+      shift
+      WORKSPACE_DIR="$1"
+      ;;
+    -d | --debug)
+      set -x
+      ;;
+    -h | --help)
+      usage
+      exit 0
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      usage
+      exit 1
+      ;;
+    esac
+    shift
+  done
+}
+
+exit_error() {
+  printf "\n[ERROR] %s\n" "$@" >&2
+  printf "Exiting script.\n"
+  exit 1
 }
 
 # populate clusters with the cluster names taken from the kubeconfig
@@ -45,92 +83,78 @@ get_clusters() {
     contexts=()
     kubeconfigs=()
     printf "Extracting files under the kubeconfig dir and reading the content in each file \n"
-    files=("$(ls "$DATA_DIR/gitops/sre/credentials/kubeconfig/compute")")
+    files=("$(ls "$WORKSPACE_DIR/gitops/sre/credentials/kubeconfig/compute")")
     for kubeconfig in "${files[@]}"; do
-        printf "%s\n" "$kubeconfig"
-        subs=("$(KUBECONFIG=${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfig} kubectl config view -o jsonpath='{range .contexts[*]}{.name}{","}{.context.cluster}{"\n"}{end}')")
+        printf "  - %s\n" "$kubeconfig"
+        subs=("$(KUBECONFIG=${WORKSPACE_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfig} kubectl config view -o jsonpath='{range .contexts[*]}{.name}{","}{.context.cluster}{"\n"}{end}')")
         for sub in "${subs[@]}"; do
             context=$(echo -n "${sub}" | cut -d ',' -f 1)
             cluster=$(echo -n "${sub}" | cut -d ',' -f 2 | cut -d ':' -f 1)
-	    if ! (echo "${clusters[@]}" | grep "${cluster}"); then
+            if ! (echo "${clusters[@]}" | grep "${cluster}"); then
                 clusters+=( "${cluster}" )
                 contexts+=( "${context}" )
                 kubeconfigs+=( "${kubeconfig}" )
-                printf "%s --- %s --- %s \n" "$cluster" "$context" "$kubeconfig"
+                printf "    - %s --- %s --- %s \n" "$cluster" "$context" "$kubeconfig"
             fi
         done
     done
+    printf "\n"
+}
 
+switch_cluster() {
+  # Sometimes the workspace is read-only, preventing the context switch
+  export KUBECONFIG="/tmp/cluster.kubeconfig"
+  cp "${WORKSPACE_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" "$KUBECONFIG"
+
+  if ! kubectl config use-context "${contexts[$i]}" >/dev/null; then
+    exit_error "\nCannot use '${contexts[$i]}' context in '$KUBECONFIG'."
+  fi
 }
 
 install_tektoncd() {
   printf "Installing tektoncd components on the cluster via Openshift GitOps... \n"
   for i in "${!clusters[@]}"; do
-    KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" apply -k "${DATA_DIR}/gitops/argocd/"
+    printf "  - %s:\n" "${clusters[$i]}"
+    switch_cluster
+    CONFIG_DIR=$(find "${WORKSPACE_DIR}/gitops/sre/environment/compute" -type d -name "${clusters[$i]}")
+    kubectl apply -k "$CONFIG_DIR"
+  done
+  printf "\n"
+}
+
+wait_for_resource() {
+  args=( "$@" )
+  while ! kubectl get "${args[@]}" >/dev/null 2>/dev/null; do
+    sleep 10
   done
 }
 
+wait_for_pod() {
+  args=( "$@" )
+  export -f wait_for_resource
+  if ! timeout 20s bash -c "wait_for_resource pods ${args[*]}"; then
+      exit_error "Pod not found."
+  fi
+  podname="$(kubectl get pods "${args[@]}" -o jsonpath='{.items[0].metadata.name}')"
+  kubectl wait --for=condition=Ready "pod/$podname" -n openshift-pipelines --timeout=60s >/dev/null
+}
+
 postchecks() {
+  printf "Checking deployment\n"
   #checking if the pipelines and triggers pods are up and running
   for i in "${!clusters[@]}"; do
-    #pipelines controller
-    declare pipelines_podname=""
-    printf "Checking if the Pipelines pod is available and Ready"
-    declare -i t=0
-    declare -i timeout=600
-    until [[ -n $pipelines_podname ]] ; do
-      (( t+=10 ))
-      if [[ "${t}" -gt "${timeout}" ]]; then
-        printf "\nPipelines pod not found. Exiting the script! \n"
-        exit 1
-      fi
-      printf "."
-      pipelines_podname=$(KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" get pods --ignore-not-found -n openshift-pipelines -l=app=tekton-pipelines-controller -o jsonpath='{.items[0].metadata.name}')
-      sleep 10
+    switch_cluster
+    printf "  - %s:\n" "${clusters[$i]}"
+    for pod in tekton-pipelines-controller tekton-triggers-controller tekton-triggers-core-interceptors; do
+      printf "    - %s: " "$pod"
+      wait_for_pod -n openshift-pipelines -l=app="$pod"
+      printf "OK\n"
     done
-
-    KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" wait --for=condition=Ready "pod/$pipelines_podname" -n openshift-pipelines --timeout=60s
-
-    #triggers controller
-    declare triggers_podname=""
-    printf "\nChecking if the Triggers pod is available and Ready"
-    declare -i t=0
-    declare -i timeout=600
-    until [[ -n $triggers_podname ]] ; do
-      (( t+=10 ))
-      if [[ "${t}" -gt "${timeout}" ]]; then
-        printf "\nTriggers pod not found. Exiting the script! \n"
-        exit 1
-      fi
-      printf "."
-      triggers_podname=$(KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" get pods --ignore-not-found -n openshift-pipelines -l=app=tekton-triggers-controller -o jsonpath='{.items[0].metadata.name}')
-      sleep 10
-    done
-
-    KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" wait --for=condition=Ready "pod/$triggers_podname" -n openshift-pipelines --timeout=60s
-
-    #triggers interceptor
-    declare triggers_interceptor_podname=""
-    printf "\nChecking if the Triggers interceptor pod is available and Ready"
-    declare -i t=0
-    declare -i timeout=600
-    until [[ -n $triggers_interceptor_podname ]] ; do
-      (( t+=10 ))
-      if [[ "${t}" -gt "${timeout}" ]]; then
-        printf "\nTriggers interceptor pod not found. Exiting the script! \n"
-        exit 1
-      fi
-      printf "."
-      triggers_interceptor_podname=$(KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" get pods --ignore-not-found -n openshift-pipelines -l=app=tekton-triggers-core-interceptors -o jsonpath='{.items[0].metadata.name}')
-      sleep 10
-    done
-
-    KUBECONFIG="${DATA_DIR}/gitops/sre/credentials/kubeconfig/compute/${kubeconfigs[$i]}" kubectl --context "${contexts[$i]}" wait --for=condition=Ready "pod/$triggers_interceptor_podname" -n openshift-pipelines --timeout=60s
   done
 }
 
 main() {
-  prechecks
+  parse_args "$@"
   get_clusters
   install_tektoncd
   postchecks
