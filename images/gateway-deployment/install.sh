8616474b44b2e4c2aef13e0161c9a8e437d85dbc
Moemen MHEDHBI <mmhedhbi@redhat.com>
2022-10-17 15:17:29 +0200

    Deploy gateway in the same SRE Workspace
    Instead of deploying gateway on a dedicated Workspace, we can have it in
    the same SRE workspace as compute but in a different namespace

--- images/gateway-deployment/install.sh
+++ images/gateway-deployment/install.sh
@@ -24,19 +24,11 @@ usage() {
 
     # Parameters
     printf "The following parameters need to be passed to the script:\n"
-    printf "KCP_ORG: the organistation for which the workload clusters need to be registered, i.e.: root:pipeline-service\n"
-    printf "KCP_WORKSPACE: the name of the workspace where the gateway gets deployed (created if it does not exist), i.e: gateway.\n"
+    printf "KCP_WORKSPACE: the name of the workspace where the gateway gets deployed, i.e: root:pipeline-service.\n"
     printf "DATA_DIR: the location of the cluster files\n"
 }
 
 prechecks () {
-    KCP_ORG="${KCP_ORG:-}"
-    if [[ -z "${KCP_ORG}" ]]; then
-        printf "KCP_ORG not set\n\n"
-        usage
-        exit 1
-    fi
-
     KCP_WORKSPACE="${KCP_WORKSPACE:-}"
     if [[ -z "${KCP_WORKSPACE}" ]]; then
         printf "KCP_WORKSPACE not set\n\n"
@@ -63,31 +55,17 @@ kcp_kubeconfig() {
     kcp_kcfg="${files[0]}"
 }
 
-switch_org() {
-    KUBECONFIG=${kcp_kcfg} kubectl kcp workspace use "${KCP_ORG}"
-    if ! (KUBECONFIG=${kcp_kcfg} kubectl api-resources >> /dev/null 2>&1); then
-        printf "%s is not a valid organization, wrong kubectl context in use or connectivity issue\n" "${KCP_ORG}"
-	usage
-	exit 1
-    fi
-}
 
 switch_ws() {
-    if (KUBECONFIG=${kcp_kcfg} kubectl get workspaces -o name | grep "${KCP_WORKSPACE}"); then
-        printf "use existing workspace\n"
-	KUBECONFIG=${kcp_kcfg} kubectl kcp workspace use "${KCP_WORKSPACE}"
-
-    else
-       printf "creating workspace %s\n" "${KCP_WORKSPACE}"
-       KUBECONFIG=${kcp_kcfg} kubectl kcp workspace create "${KCP_WORKSPACE}" --type=universal --enter
-    fi
+  KUBECONFIG=${kcp_kcfg} kubectl kcp workspace use "${KCP_WORKSPACE}"
+  if ! (KUBECONFIG=${kcp_kcfg} kubectl api-resources >> /dev/null 2>&1); then
+    printf "%s is not a valid workspace, wrong kubectl context in use or connectivity issue\n" "${KCP_WORKSPACE}"
+    usage
+    exit 1
+  fi
 }
 
-bind_apis() {
-    CONFIG_DIR="${DATA_DIR}/environment/kcp/gateway-bindings"
-    KUBECONFIG=${kcp_kcfg} kubectl apply -k "${CONFIG_DIR}"
-}
-    
+
 install_gateway() {
     CONFIG_DIR="${DATA_DIR}/environment/kcp/gateway"
     KUBECONFIG=${kcp_kcfg} kubectl apply -k "${CONFIG_DIR}"
@@ -96,14 +74,8 @@ install_gateway() {
 main() {
     prechecks
     kcp_kubeconfig
-    printf "Switching to organization %s\n" "${KCP_ORG}"
-    switch_org
     printf "Switching to workspace %s\n" "${KCP_WORKSPACE}"
     switch_ws
-    printf "Bindind APIs\n"
-    bind_apis
-    # Give time for the API binding to happen
-    sleep 1
     printf "Installing gateway\n"
     install_gateway
 }
