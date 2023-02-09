1f93255abce8eb670b702ddc29aad3afaeb571a7
Xin Jiang <xjiang@redhat.com>
2023-02-09 13:09:12 +0800

    PLNSRVCE-1098: provision and destroy hypershift by using hyperhisft cli

--- ci/images/ci-runner/hack/bin/deploy-cluster.sh
+++ ci/images/ci-runner/hack/bin/deploy-cluster.sh
@@ -2,34 +2,50 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-set -x
 
-kubectl -n "$NAMESPACE" apply -f "$WORKSPACE/hypershift_deployment.yaml"
-echo "Wait until hypershift cluster is ready..."
-wait_period=0
-while
-    [ \
-        "$(
-            kubectl -n "$NAMESPACE" get hypershiftdeployment "$CLUSTER_NAME" -o json \
-                | jq -r '
-                    .status.conditions[]? 
-                    | select(.type == "HostedClusterProgress") 
-                    | .reason
-                '
-        )" != "Completed" \
-    ]; do
-    if [ "$wait_period" -gt 1200 ]; then
-        echo "[ERROR] Failed to create OCP cluster." >&2
-        kubectl -n "$NAMESPACE" get hypershiftdeployment "$CLUSTER_NAME" -o yaml
-        exit 1
-    fi
-    sleep 60
-    wait_period=$((wait_period + 60))
-    echo "Waited $wait_period seconds..."
-done
+SCRIPT_DIR="$(
+  cd "$(dirname "$0")" >/dev/null
+  pwd
+)"
 
-echo "Hypershift is ready, the following is the cluster kubeconfig"
-kubectl get secret -n clusters "$CLUSTER_NAME-admin-kubeconfig" -o json |
-    jq -r '.data.kubeconfig' |
-    base64 -d |
-    tee "$WORKSPACE/kubeconfig"
+# shellcheck source=ci/images/ci-runner/hack/bin/utils.sh
+source "$SCRIPT_DIR/utils.sh"
+
+fetch_bitwarden_secrets() {
+    printf "Fetch secrets from bitwarden server\n" | indent 2
+    open_bitwarden_session
+    get_base_domain
+    get_pull_secret
+    get_aws_credentials
+}
+
+deploy_cluster() {
+    setx_off
+    hypershift create cluster aws --pull-secret "$PULL_SECRET" --aws-creds "$AWS_CREDENTIALS"  --name "$CLUSTER_NAME"  --node-pool-replicas=2  --base-domain "$BASE_DOMAIN"  --region="$REGION" --release-image="$IMAGE" --root-volume-type=gp3 --root-volume-size=60 --instance-type=m5.xlarge
+    setx_on
+
+    echo "Wait until hypershift hosted cluster is ready..."
+    wait_period=0
+    while
+        [ \
+            "$(
+                kubectl -n clusters get hostedcluster "$CLUSTER_NAME" -o json \
+                    | jq -r '.status.version.history[0].state'
+            )" != "Completed" \
+        ]; do
+        if [ "$wait_period" -gt 1800 ]; then
+            echo "[ERROR] Failed to create OCP cluster." >&2
+            kubectl -n clusters get hostedcluster "$CLUSTER_NAME" -o yaml
+            exit 1
+        fi
+        sleep 60
+        wait_period=$((wait_period + 60))
+        echo "Waited $wait_period seconds..."
+    done
+
+    echo "Hypershift is ready, the following is the cluster kubeconfig"
+    hypershift create kubeconfig --name "$CLUSTER_NAME" | tee "$WORKSPACE/kubeconfig"
+}
+
+fetch_bitwarden_secrets
+deploy_cluster
--- ci/images/ci-runner/hack/bin/destroy-clusters.sh
+++ ci/images/ci-runner/hack/bin/destroy-clusters.sh
@@ -4,32 +4,39 @@ set -o nounset
 set -o pipefail
 set -x
 
+SCRIPT_DIR="$(
+  cd "$(dirname "$0")" >/dev/null
+  pwd
+)"
+
+# shellcheck source=ci/images/ci-runner/hack/bin/utils.sh
+source "$SCRIPT_DIR/utils.sh"
+
 MAX_DURATION_MINS=60
 EXCLUDE_CLUSTER=(local-cluster)
 
 delete_cluster() {
     cluster_name="$1"
-    status=$(kubectl -n ci-clusters get hd "$cluster_name" -o json \
-        | jq -r '
-            .status.conditions[]? 
-            | select(.type == "ManifestWorkConfigured") 
-            |.reason
-        '
+    deletionTimestamp=$(kubectl get hostedcluster -n clusters "$cluster_name" -o json \
+        | jq -r '.metadata.deletionTimestamp?'
     )
-    # If the the cluster is Removing state, it won't delete this cluster
-    if [ "$status" = "Removing" ]; then
+
+    # If the the cluster has "deletionTimestamp" metadata, it means the cluster is triggered deletion
+    if [ -z "$deletionTimestamp" ]; then
         return
     fi
     # If the cluster is older than $MAX_DURATION_MINS mins, it will be deleted
-    creationTime=$(kubectl -n ci-clusters get hd "$cluster_name" -o json | jq -r '.metadata.creationTimestamp')
+    creationTime=$(kubectl -n clusters get hostedcluster "$cluster_name" -o json | jq -r '.metadata.creationTimestamp')
     durations_mins=$((($(date +%s) - $(date +%s -d "$creationTime")) / 60))
     if [ "$durations_mins" -gt "$MAX_DURATION_MINS" ]; then
         echo "Start delete cluster $cluster_name"
-        kubectl -n ci-clusters delete hd "$cluster_name"
+        open_bitwarden_session
+        get_aws_credentials
+        hypershift destroy cluster aws --aws-creds "$AWS_CREDENTIALS"  --name "$cluster_name"
     fi
 }
 
-mapfile -t clusters < <(kubectl get managedcluster -o=custom-columns=NAME:.metadata.name --no-headers)
+mapfile -t clusters < <(kubectl get hostedcluster -n clusters -o=custom-columns=NAME:.metadata.name --no-headers)
 for cluster in "${clusters[@]}"; do
     if [[ "${EXCLUDE_CLUSTER[*]}" =~ $cluster ]]; then
         continue
