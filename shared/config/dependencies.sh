3c13523c0370df605e602fb1c9c22a08cbf7b363
Romain Arnaud <rarnaud@redhat.com>
2023-02-27 16:51:53 -0500

    Add mechanism to freeze a dependency version
    The change also fixes a bug that was forcing the HyperShift version
    to be set with the hadolint version.
    
    Signed-off-by: Romain Arnaud <rarnaud@redhat.com>

--- shared/config/dependencies.sh
+++ shared/config/dependencies.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+# To prevent a version to be upgraded by 'update_binaries.sh',
+# add '# Freeze' after the export.
+# E.g.:
+#       export FOOBAR_VERSION="1.2.3"  # Freeze
 export ARGOCD_VERSION="v2.5.7"
 export BITWARDEN_VERSION="v2023.1.0"
 export CHECKOV_VERSION="2.2.281"
 export GO_VERSION="1.19.5"
 export HADOLINT_VERSION="v2.12.0"
-export HYPERSHIFT_VERSION="main"
+export HYPERSHIFT_VERSION="main"  # Freeze
 export JQ_VERSION="1.6"
 export KIND_VERSION="v0.17.0"
 export KUBECTL_VERSION="v1.26.1"
