8f3ebf86101a4cf21fe7e86335e85a3fb6e3671f
Pipeline Service CI Robot <pipeline-service@example.com>
2023-03-15 13:53:19 +0000

    Update binaries - argocd to v2.6.5 - checkov to 2.3.95 - go to 1.20.2 - kubectl to v1.26.2 - oc to 4.12.7 - yq to v4.31.2
    Signed-off-by: Pipeline Service CI Robot <pipeline-service@example.com>

--- shared/config/dependencies.sh
+++ shared/config/dependencies.sh
@@ -3,17 +3,17 @@
 # add '# Freeze' after the export.
 # E.g.:
 #       export FOOBAR_VERSION="1.2.3"  # Freeze
-export ARGOCD_VERSION="v2.6.3"
+export ARGOCD_VERSION="v2.6.5"
 export BITWARDEN_VERSION="v2023.2.0"
-export CHECKOV_VERSION="2.3.50"
-export GO_VERSION="1.20.1"
+export CHECKOV_VERSION="2.3.95"
+export GO_VERSION="1.20.2"
 export HADOLINT_VERSION="v2.12.0"
 export HYPERSHIFT_VERSION="main"  # Freeze
 export JQ_VERSION="1.6"
 export KIND_VERSION="v0.17.0"
-export KUBECTL_VERSION="v1.26.1"
-export OC_VERSION="4.12.5"
+export KUBECTL_VERSION="v1.26.2"
+export OC_VERSION="4.12.7"
 export SHELLCHECK_VERSION="v0.9.0"
 export TEKTONCD_CLI_VERSION="v0.29.1"
 export YAMLLINT_VERSION="1.29.0"
-export YQ_VERSION="v4.31.1"
+export YQ_VERSION="v4.31.2"
