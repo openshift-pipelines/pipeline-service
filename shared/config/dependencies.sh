09ca47cfb8d12fe6cc54c573b9b8e9bc560f881e
Pipeline Service CI Robot <pipeline-service@example.com>
2023-03-21 19:46:11 +0000

    Update binaries - checkov to 2.3.108 - oc to 4.12.8
    Signed-off-by: Pipeline Service CI Robot <pipeline-service@example.com>

--- shared/config/dependencies.sh
+++ shared/config/dependencies.sh
@@ -5,14 +5,14 @@
 #       export FOOBAR_VERSION="1.2.3"  # Freeze
 export ARGOCD_VERSION="v2.6.6"
 export BITWARDEN_VERSION="v2023.2.0"
-export CHECKOV_VERSION="2.3.106"
+export CHECKOV_VERSION="2.3.108"
 export GO_VERSION="1.20.2"
 export HADOLINT_VERSION="v2.12.0"
 export HYPERSHIFT_VERSION="main"  # Freeze
 export JQ_VERSION="1.6"
 export KIND_VERSION="v0.17.0"
 export KUBECTL_VERSION="v1.26.3"
-export OC_VERSION="4.12.7"
+export OC_VERSION="4.12.8"
 export SHELLCHECK_VERSION="v0.9.0"
 export TEKTONCD_CLI_VERSION="v0.29.1"
 export YAMLLINT_VERSION="1.29.0"
