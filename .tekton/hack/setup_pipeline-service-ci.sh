b28e62b07abc9dc0d64b01370268c4f622f830c2
Romain Arnaud <rarnaud@redhat.com>
2023-03-28 10:45:19 -0400

    Update the key for github.com
    Signed-off-by: Romain Arnaud <rarnaud@redhat.com>

--- .tekton/hack/setup_pipeline-service-ci.sh
+++ .tekton/hack/setup_pipeline-service-ci.sh
@@ -85,7 +85,7 @@ generate_ssh_key() {
 setup_ssh_secret_directory() {
     mv "$TMPDIR/secret/ssh-privatekey.pub" "$TMPDIR/secret/ssh-publickey"
     cat <<EOF >"$TMPDIR/secret/known_hosts"
-github.com,140.82.112.3 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+github.com ssh-rsa AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
 EOF
 }
 
