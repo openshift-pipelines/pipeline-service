0d4224b4b93936da821e32eb313438d187e8ab2a
Pipeline Service CI Robot <pipeline-service@example.com>
2023-04-17 16:44:37 +0000

    Update binaries - checkov to 2.3.176 - kubectl to v1.27.1
    Signed-off-by: Pipeline Service CI Robot <pipeline-service@example.com>

--- shared/config/dependencies.sh
+++ shared/config/dependencies.sh
@@ -5,13 +5,13 @@
 #       export FOOBAR_VERSION="1.2.3"  # Freeze
 export ARGOCD_VERSION="v2.6.7"
 export BITWARDEN_VERSION="v2023.3.0"
-export CHECKOV_VERSION="2.3.165"
+export CHECKOV_VERSION="2.3.176"
 export GO_VERSION="1.20.3"
 export HADOLINT_VERSION="v2.12.0"
 export HYPERSHIFT_VERSION="main"  # Freeze
 export JQ_VERSION="1.6"
 export KIND_VERSION="v0.18.0"
-export KUBECTL_VERSION="v1.27.0"
+export KUBECTL_VERSION="v1.27.1"
 export OC_VERSION="4.12.12"
 export SHELLCHECK_VERSION="v0.9.0"
 export TEKTONCD_CLI_VERSION="v0.30.1"
