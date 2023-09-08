29b16e66730823836dbdba239f4ab40a5961b099
Xin Jiang <xjiang@redhat.com>
2023-09-08 18:27:06 +0800

    added a task copy-plnsvc-code

--- ci/images/ci-runner/hack/bin/copy-plnsvc-code.sh
+++ ci/images/ci-runner/hack/bin/copy-plnsvc-code.sh
@@ -5,7 +5,9 @@ set -o pipefail
 set -x
 
 echo "Copy source code of pipeline service to the ci-runner container"
+kubectl exec -n default -it ci-runner -- bash -c "rm -rf /source/*"
 kubectl cp "./" "default/ci-runner:/source"
 
 echo "Copy new cluster's kubeconfig to the ci-runner container"
+kubectl exec -n default -it ci-runner -- bash -c "rm -rf /kubeconfig"
 kubectl cp "$KUBECONFIG" "default/ci-runner:/kubeconfig"
