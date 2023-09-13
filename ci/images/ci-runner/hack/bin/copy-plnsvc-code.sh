573268b9d5af7c8ebb00d389933a6bbe62f465af
Romain Arnaud <rarnaud@redhat.com>
2023-09-13 14:51:56 -0400

    Fix upgrade-tests pipeline
    The wrong set of scripts were used when deploying/testing the
    baseline branch. Scripts from the baseline branch are now used
    when testing the baseline branch, and scripts from the PR are
    used when testing the PR branch.
    
    However CI related scripts always use the source branch, so that any
    changes in the CI scripts and/or pipeline definitions work as expected.
    
    * Fix issue with a sidecar script that was not called from the sidecar.
    * Change target of source code copy to /workspace/source on the sidecar
    * Copy sidecar scripts to /workspace/sidecar on the sidecar
    * Checkout the revision being tested before setup and test
    * Rename ci-runner-container-setup.yaml to setup-ci-runner-container.yaml
    so filename and taskname match
    
    Signed-off-by: Romain Arnaud <rarnaud@redhat.com>

--- ci/images/ci-runner/hack/bin/copy-plnsvc-code.sh
+++ ci/images/ci-runner/hack/bin/copy-plnsvc-code.sh
@@ -5,7 +5,10 @@ set -o pipefail
 set -x
 
 echo "Copy source code of pipeline service to the ci-runner container"
-kubectl cp "./" "default/ci-runner:/source"
+kubectl cp "./" "default/ci-runner:/workspace/source"
+
+echo "Copy sidecar sources to the ci-runner container"
+kubectl cp "./ci/images/ci-runner/hack/sidecar" "default/ci-runner:/workspace/sidecar"
 
 echo "Copy new cluster's kubeconfig to the ci-runner container"
 kubectl cp "$KUBECONFIG" "default/ci-runner:/kubeconfig"
--- ci/images/ci-runner/hack/sidecar/bin/plnsvc_test.sh
+++ ci/images/ci-runner/hack/sidecar/bin/plnsvc_test.sh
@@ -6,11 +6,4 @@ set -x
 
 echo "Start executing pipeline cases ..."
 TEST_DIR=$(find "$PWD" -type f -name test.sh -exec dirname {} +)
-"$TEST_DIR/test.sh" --kubeconfig "$KUBECONFIG"
-
-
-# In case the user deleted the file early when they expected a failure
-if [ -e "$PWD/destroy-cluster.txt" ]; then
-    # If the tests are successful, the cluster can be destroyed right away
-    rm "$PWD/destroy-cluster.txt"
-fi
+"$TEST_DIR/test.sh" --kubeconfig "/kubeconfig"
