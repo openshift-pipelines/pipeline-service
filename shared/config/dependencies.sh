6325e2811beb5045793f326cca90c6fb6a5e96c3
Tekton CI <tekton-ci@example.com>
2024-01-25 22:37:46 +0000

    Update binaries
    - argocd to v2.9.5
    - checkov to 3.1.70
    - kubectl to v1.29.1
    - oc to 4.14.10
    - terraform to 1.7.1
    
    Signed-off-by: Tekton CI <tekton-ci@example.com>

--- shared/config/dependencies.sh
+++ shared/config/dependencies.sh
@@ -3,17 +3,17 @@
 # add '# Freeze' after the export.
 # E.g.:
 #       export FOOBAR_VERSION="1.2.3"  # Freeze
-export ARGOCD_VERSION="v2.9.3"
+export ARGOCD_VERSION="v2.9.5"
 export BITWARDEN_VERSION="v2024.1.0"
-export CHECKOV_VERSION="3.1.57"
+export CHECKOV_VERSION="3.1.70"
 export GO_VERSION="1.21.6"
 export HADOLINT_VERSION="v2.12.0"
 export JQ_VERSION="1.7.1"
-export KUBECTL_VERSION="v1.29.0"
-export OC_VERSION="4.14.8"
+export KUBECTL_VERSION="v1.29.1"
+export OC_VERSION="4.14.10"
 export ROSA_VERSION="1.2.33"
 export SHELLCHECK_VERSION="v0.9.0"
 export TEKTONCD_CLI_VERSION="v0.34.0"
-export TERRAFORM_VERSION="1.6.6"
+export TERRAFORM_VERSION="1.7.1"
 export YAMLLINT_VERSION="1.33.0"
 export YQ_VERSION="v4.40.5"
